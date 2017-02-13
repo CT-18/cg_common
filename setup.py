874f9e0c8768b8a83b60992214ae4fcf2b917d06
sandwwraith <sandwwraith@gmail.com>
2017-02-14 02:48:08 +0300

    Coverage setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='cg',
-      version='0.1a',
+      version='0.1',
       description='common tools for computational geometry notebooks',
       author='Artem Zholus',
       author_email='artem.zholus@gmail.com',
       url='https://github.com/CT-18/cg_common',
+      test_suite='test',
       requires=['numpy'],
       packages=['cg'])
